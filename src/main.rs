5cd67d1927172faa8fb097bd4103a2f4e7f68f27
Frank Denis <github@pureftpd.org>
2021-11-28 16:53:25 +0100

    up

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fs;
+use std::io;
+use std::path::Path;
+use wasmsign2::reexports::thiserror;
 use wasmsign2::*;
+
+#[derive(Debug, thiserror::Error)]
+pub enum WSRError {
+    #[error("Internal error: [{0}]")]
+    InternalError(String),
+    #[error("Configuration error: [{0}]")]
+    ConfigError(String),
+    #[error("I/O error: [{0}]")]
+    IOError(#[from] io::Error),
+    #[error("YAML error: [{0}]")]
+    YAMLError(#[from] serde_yaml::Error),
+    #[error("WASMSign error: [{0}]")]
+    WSError(#[from] wasmsign2::WSError),
+}
 
 mod raw {
     use super::*;
@@ -51,67 +68,91 @@ struct Rule {
 }
 
 #[derive(Debug, Clone)]
-struct Rules {
+pub struct Rules {
     signers: BTreeMap<String, PublicKeySet>,
     required_signatures: Vec<Rule>,
     rejected_signatures: Vec<Rule>,
 }
 
-fn main() {
-    let yaml = fs::read_to_string("/tmp/a.yaml").unwrap();
-    let rules_raw: raw::Rules = serde_yaml::from_str(&yaml).unwrap();
-    dbg!(&rules_raw);
-
-    let mut signers = BTreeMap::new();
-    for (signers_name_raw, signers_raw) in rules_raw.signers {
-        let mut pks = PublicKeySet::empty();
-        for signer_raw in signers_raw {
-            let pk = PublicKey::from_file(&signer_raw.file).unwrap();
-            pks.insert(pk).unwrap();
-        }
-        signers.insert(signers_name_raw, pks);
-    }
-
+fn signature_rules(
+    required_signatures_raw: &Option<Vec<raw::Rule>>,
+    signers: &BTreeMap<String, PublicKeySet>,
+) -> Result<Vec<Rule>, WSRError> {
     let mut required_signatures = vec![];
-    for rule_raw in rules_raw.required_signatures.unwrap_or_default() {
+    let required_signatures_raw = match required_signatures_raw {
+        Some(rules) => rules,
+        None => return Ok(required_signatures),
+    };
+    for rule_raw in required_signatures_raw {
         let mut sections = vec![];
-        for section_raw in rule_raw.sections {
+        for section_raw in &rule_raw.sections {
             match section_raw.r#type.as_str() {
                 "standard" => {
                     sections.push(RequiredSections::StandardSections);
                 }
                 "custom" => {
-                    if let Some(rx) = section_raw.matching {
+                    if let Some(rx) = &section_raw.matching {
                         sections.push(RequiredSections::CustomSections(
-                            RequiredCustomSections::Regex(rx),
+                            RequiredCustomSections::Regex(rx.clone()),
                         ));
-                    } else if let Some(name) = section_raw.eq {
+                    } else if let Some(name) = &section_raw.eq {
                         sections.push(RequiredSections::CustomSections(
-                            RequiredCustomSections::Eq(name),
+                            RequiredCustomSections::Eq(name.clone()),
                         ));
                     }
                 }
-                _ => panic!("Unexpected custom section name matcher"),
+                x => {
+                    return Err(WSRError::ConfigError(format!(
+                        "Unexpected matcher name for a section set: [{}]",
+                        x
+                    )))
+                }
             }
         }
         for signer_name in &rule_raw.signers_names {
             if !signers.contains_key(signer_name) {
-                panic!("Signer name not defined");
+                return Err(WSRError::ConfigError(format!(
+                    "Signer name not defined: [{}]",
+                    signer_name
+                )));
             }
         }
         let rule = Rule {
             sections,
-            signers_names: rule_raw.signers_names,
+            signers_names: rule_raw.signers_names.clone(),
         };
         required_signatures.push(rule);
     }
+    Ok(required_signatures)
+}
 
-    let rejected_signatures = vec![];
+impl Rules {
+    pub fn from_yaml_file(file: impl AsRef<Path>) -> Result<Rules, WSRError> {
+        let yaml = fs::read_to_string(file.as_ref())?;
+        let rules_raw: raw::Rules = serde_yaml::from_str(&yaml)?;
 
-    let rules = Rules {
-        signers,
-        required_signatures,
-        rejected_signatures,
-    };
-    dbg!(rules);
+        let mut signers = BTreeMap::new();
+        for (signers_name_raw, signers_raw) in rules_raw.signers {
+            let mut pks = PublicKeySet::empty();
+            for signer_raw in signers_raw {
+                let pk = PublicKey::from_file(&signer_raw.file)?;
+                pks.insert(pk)?;
+            }
+            signers.insert(signers_name_raw, pks);
+        }
+
+        let required_signatures = signature_rules(&rules_raw.required_signatures, &signers)?;
+        let rejected_signatures = signature_rules(&rules_raw.rejected_signatures, &signers)?;
+
+        let rules = Rules {
+            signers,
+            required_signatures,
+            rejected_signatures,
+        };
+        Ok(rules)
+    }
+}
+
+fn main() {
+    Rules::from_yaml_file("/tmp/a.yaml").unwrap();
 }
